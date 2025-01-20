2e795c7d1242b9f7fffcd758b78917626fc3fd31
Ian <ianpike98@gmail.com>
2025-01-19 23:24:08 -0500

    Even more adjustment of the current structure

--- launcher/src/main.cpp
+++ launcher/src/main.cpp
@@ -4,9 +4,21 @@
 #include <steam/steam_api.h>
 #include <iostream>
 
+#ifndef FORNANI_STEAM_APP_ID
+#error "FORNANI_STEAM_APP_ID was not defined!"
+#endif
+
+#if defined(FORNANI_STEAM_APP_ID) && FORNANI_STEAM_APP_ID < 0
+#error "FORNANI_STEAM_APP_ID was defined as a negative number!"
+#endif
+
 int main(int argc, char** argv) {
 	assert(argc > 0);
 	fornani::Application app{argv};
+
+
+
+	std::cout << "Current passed steam ID: " << FORNANI_STEAM_APP_ID << std::endl;
 
 	if (SteamAPI_RestartAppIfNecessary(FORNANI_STEAM_APP_ID)) {
 		std::cout << "Re-launching through Steam." << std::endl;
