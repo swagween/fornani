30f3f731934d5586520a405d77536636b371a677
Ian <ianpike98@gmail.com>
2025-01-22 20:10:39 -0500

    Reduce the coupling of Application and what is launching it

--- game/include/fornani/setup/Application.hpp
+++ game/include/fornani/setup/Application.hpp
@@ -8,21 +8,22 @@ namespace fornani {
 
 class Application {
   public:
-	explicit Application(char** argv, const char * loading_screen = "/image/gui/loading.png")  : finder(argv), t_loading(finder.resource_path + loading_screen), loading(t_loading) {};
+	explicit Application(char** argv, const char * loading_screen = "/image/gui/loading.png")  : m_finder(argv), m_t_loading(m_finder.resource_path + loading_screen), m_loading(m_t_loading) {};
 	virtual ~Application() = default;
-	virtual void init(char** argv);
-	virtual void launch(char** argv, bool demo = false, int room_id = 100, std::filesystem::path levelpath = std::filesystem::path{}, sf::Vector2<float> player_position = {});
+	virtual void init(char** argv){};
+
+	virtual void launch(char** argv, bool demo = false, int room_id = 100, std::filesystem::path levelpath = std::filesystem::path{}, sf::Vector2<float> player_position = {}){};
 
 
   protected:
 
-	data::ResourceFinder finder;
-	dj::Json game_info{};
-	dj::Json app_settings{};
-	Version metadata{};
-	WindowManager window{};
-	sf::Texture t_loading{};
-	sf::Sprite loading;
+	data::ResourceFinder m_finder;
+	dj::Json m_game_info{};
+	dj::Json m_app_settings{};
+	Version m_metadata{};
+	WindowManager m_window{};
+	sf::Texture m_t_loading{};
+	sf::Sprite m_loading;
 };
 
 } // namespace fornani
--- launcher/include/launcher/LauncherApplication.hpp
+++ launcher/include/launcher/LauncherApplication.hpp
@@ -1,10 +1,15 @@
-
 #pragma once
 
 #include "fornani/setup/Application.hpp"
 
+namespace game {
+class LauncherApplication : public fornani::Application {
+public:
+	LauncherApplication(char** argv, const char* loading_screen = "/image/gui/loading.png") : Application(argv, loading_screen) {};
+	void init(char** argv) override;
+	void launch(char** argv, bool demo = false, int room_id = 100, std::filesystem::path levelpath = std::filesystem::path{}, sf::Vector2<float> player_position = {}) override;
 
-class LauncherApplication : public fornani::Application
-{
-
+private:
+	fornani::Logger m_logger{"Application"};
 }; // class LauncherApplication
+}
