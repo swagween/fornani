1f19765afaa8f35c1f3030eb1b825fd75f128e15
Ian Pike <Ianpike98@gmail.com>
2025-02-12 16:06:00 -0500

    Bug fixing and general cleanup

--- game/include/fornani/core/Event.hpp
+++ game/include/fornani/core/Event.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
-#include <vector>
-#include <shared_mutex>
-#include <atomic>
-#include <memory>
-#include <mutex>
 #include <algorithm>
 #include <cassert>
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
 #include <unordered_map>
+#include <vector>
 
 // design based on Impossibly Fast Event:
 // https://docs.google.com/presentation/d/1Y5JktAemPYNDmVJ5-3f_KQH1LTDmey-YJFAnly0Cxpo/edit?usp=sharing
@@ -35,20 +34,20 @@ struct EventBase {
 
 	EventBase() = default;
 	~EventBase();
-	EventBase(const EventBase&) = delete;
-	EventBase& operator=(const EventBase&) = delete;
+	EventBase(EventBase const&) = delete;
+	EventBase& operator=(EventBase const&) = delete;
 	EventBase(EventBase&& other) noexcept;
 	EventBase& operator=(EventBase&& other) noexcept;
 };
 
 struct BlockedConnection {
-	const EventBase* sig = nullptr;
-	EventBase::call call = {nullptr, nullptr};
+	EventBase const* sig = nullptr;
+	EventBase::call call = {.object = nullptr, .func = nullptr};
 };
 
 struct ConnectionBase {
 	union {
-		const EventBase* event;
+		EventBase const* event;
 		BlockedConnection* blocked_conn;
 	};
 
@@ -58,42 +57,49 @@ struct ConnectionBase {
 	bool blocked = false;
 	bool owned = false;
 
-	ConnectionBase(const EventBase* sig, size_t idx) : event(sig), idx(idx) {}
+	ConnectionBase(EventBase const* sig, size_t idx) : event(sig), idx(idx) {}
 
 	virtual ~ConnectionBase() {
 		if (!blocked) {
-			if (event) {
+			// NOLINTBEGIN
+			if (event != nullptr) {
 				event->calls[idx].object = nullptr;
 				event->calls[idx].func = nullptr;
 				event->connections[idx] = nullptr;
 				event->dirty = true;
 			}
-		} else { delete blocked_conn; }
+			// NOLINTEND
+		} else {
+			delete blocked_conn; // NOLINT
+		}
 	}
 
-	void set_event(const EventBase* sig) {
-		if (blocked) this->blocked_conn->sig = sig;
-		else this->event = sig;
+	void set_event(EventBase const* sig) {
+		if (blocked) {
+			this->blocked_conn->sig = sig; // NOLINT
+		} else {
+			this->event = sig; // NOLINT
+		}
 	}
 
 	void block() {
 		if (!blocked) {
 			blocked = true;
-			const EventBase* orig_sig = event;
-			event = nullptr;
-			blocked_conn = new BlockedConnection;
-			blocked_conn->sig = orig_sig;
-			std::swap(blocked_conn->call, orig_sig->calls[idx]);
+			EventBase const* orig_sig = event;					 // NOLINT
+			event = nullptr;									 // NOLINT
+			blocked_conn = new BlockedConnection;				 // NOLINT
+			blocked_conn->sig = orig_sig;						 // NOLINT
+			std::swap(blocked_conn->call, orig_sig->calls[idx]); // NOLINT
 		}
 	}
 
 	void unblock() {
 		if (blocked) {
-			const EventBase* orig_sig = blocked_conn->sig;
-			std::swap(blocked_conn->call, orig_sig->calls[idx]);
-			delete blocked_conn;
-			blocked_conn = nullptr;
-			event = orig_sig;
+			EventBase const* orig_sig = blocked_conn->sig;		 // NOLINT
+			std::swap(blocked_conn->call, orig_sig->calls[idx]); // NOLINT
+			delete blocked_conn;								 // NOLINT
+			blocked_conn = nullptr;								 // NOLINT
+			event = orig_sig;									 // NOLINT
 			blocked = false;
 		}
 	}
@@ -103,30 +109,37 @@ template <typename T>
 struct ConnectionNontrivial final : ConnectionBase {
 	using ConnectionBase::ConnectionBase;
 
-	~ConnectionNontrivial() override { if (event) reinterpret_cast<T*>(&event->calls[idx].object)->~T(); }
+	~ConnectionNontrivial() override {
+		if (event) { reinterpret_cast<T*>(&event->calls[idx].object)->~T(); } // NOLINT
+	}
 };
 
 inline EventBase::~EventBase() {
 	for (ConnectionBase* c : connections) {
-		if (c) {
-			if (c->owned) c->set_event(nullptr);
-			else delete c;
+		if (c != nullptr) {
+			if (c->owned) {
+				c->set_event(nullptr);
+			} else {
+				delete c; // NOLINT
+			}
 		}
 	}
 }
 
-inline EventBase::EventBase(EventBase&& other) noexcept
-	: calls(std::move(other.calls))
-	  , connections(std::move(other.connections))
-	  , calling(other.calling)
-	  , dirty(other.dirty) { for (ConnectionBase* c : connections) if (c) c->set_event(this); }
+inline EventBase::EventBase(EventBase&& other) noexcept : calls(std::move(other.calls)), connections(std::move(other.connections)), calling(other.calling), dirty(other.dirty) {
+	for (ConnectionBase* c : connections) {
+		if (c != nullptr) { c->set_event(this); }
+	}
+}
 
 inline EventBase& EventBase::operator=(EventBase&& other) noexcept {
 	calls = std::move(other.calls);
 	connections = std::move(other.connections);
 	calling = other.calling;
 	dirty = other.dirty;
-	for (ConnectionBase* c : connections) if (c) c->set_event(this);
+	for (ConnectionBase* c : connections) {
+		if (c != nullptr) { c->set_event(this); }
+	}
 	return *this;
 }
 } // namespace details
@@ -154,12 +167,11 @@ struct Connection {
 	Connection() = default;
 
 	~Connection() { disconnect(); }
-	Connection(const Connection&) = delete;
+	Connection(Connection const&) = delete;
 
-	Connection& operator=(const Connection&) = delete;
+	Connection& operator=(Connection const&) = delete;
 
-	Connection(Connection&& other) noexcept
-		: ptr(other.ptr) { other.ptr = nullptr; }
+	Connection(Connection&& other) noexcept : ptr(other.ptr) { other.ptr = nullptr; }
 
 	Connection& operator=(Connection&& other) noexcept {
 		disconnect();
@@ -168,20 +180,23 @@ struct Connection {
 		return *this;
 	}
 
-	Connection(ConnectionRaw conn) : ptr(conn.ptr) { ptr->owned = true; }
+	Connection(ConnectionRaw conn) : ptr(conn.ptr) { ptr->owned = true; } // NOLINT
 };
 
 template <typename... A>
 struct Event<void(A...)> : details::EventBase {
 	template <typename... ActualArgsT>
 	void operator()(ActualArgsT&&... args) const {
-		const bool recursion = calling;
-		if (!calling) calling = true;
+		bool const recursion = calling;
+		if (!calling) { calling = true; }
 		for (size_t i = 0, n = calls.size(); i < n; ++i) {
 			auto& [object, func] = calls[i];
 			if (func) {
-				if (object == func) reinterpret_cast<void(*)(A...)>(func)(std::forward<ActualArgsT>(args)...);
-				else reinterpret_cast<void(*)(void*, A...)>(func)(&object, std::forward<ActualArgsT>(args)...);
+				if (object == func) {
+					reinterpret_cast<void (*)(A...)>(func)(std::forward<ActualArgsT>(args)...); // NOLINT
+				} else {
+					reinterpret_cast<void (*)(void*, A...)>(func)(&object, std::forward<ActualArgsT>(args)...); // NOLINT
+				}
 			}
 		}
 
@@ -206,7 +221,7 @@ struct Event<void(A...)> : details::EventBase {
 		}
 	}
 
-    template <typename... ActualArgsT>
+	template <typename... ActualArgsT>
 	void invoke(ActualArgsT&&... args) const {
 		operator()(std::forward<ActualArgsT>(args)...);
 	}
@@ -214,10 +229,11 @@ struct Event<void(A...)> : details::EventBase {
 	template <auto PMF, class C>
 	ConnectionRaw connect(C* object) const {
 		size_t idx = connections.size();
+		// ReSharper disable once CppUseStructuredBinding
 		auto& call = calls.emplace_back();
 		call.object = object;
-		call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { ((*static_cast<C**>(obj))->*PMF)(args...); });
-		details::ConnectionBase* conn = new details::ConnectionBase(this, idx);
+		call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { ((*static_cast<C**>(obj))->*PMF)(args...); }); // NOLINT
+		auto* conn = new details::ConnectionBase(this, idx);														   // NOLINT
 		connections.emplace_back(conn);
 		return {conn};
 	}
@@ -228,48 +244,50 @@ struct Event<void(A...)> : details::EventBase {
 	}
 
 	template <auto func>
-	ConnectionRaw connect() const { return connect(func); }
+	ConnectionRaw connect() const {
+		return connect(func);
+	}
 
 	ConnectionRaw connect(void (*func)(A...)) const {
 		size_t idx = connections.size();
 		auto& call = calls.emplace_back();
-		call.func = call.object = reinterpret_cast<void*>(func);
-		details::ConnectionBase* conn = new details::ConnectionBase(this, idx);
+		call.func = call.object = reinterpret_cast<void*>(func); // NOLINT
+		auto* conn = new details::ConnectionBase(this, idx);	 // NOLINT
 		connections.emplace_back(conn);
 		return {conn};
 	}
 
-	ConnectionRaw operator+=(void (*func)(A...)) const {
-		return connect(func);
-	}
+	ConnectionRaw operator+=(void (*func)(A...)) const { return connect(func); }
 
 	template <typename F>
 	ConnectionRaw connect(F&& functor) const {
 		using f_type = std::remove_pointer_t<std::remove_reference_t<F>>;
-		if constexpr (std::is_convertible_v<f_type, void(*)(A...)>) { return connect(+functor); } else if constexpr (std::is_lvalue_reference_v<F>) {
+		if constexpr (std::is_convertible_v<f_type, void (*)(A...)>) {
+			return connect(+functor);
+		} else if constexpr (std::is_lvalue_reference_v<F>) {
 			size_t idx = connections.size();
 			auto& call = calls.emplace_back();
-			call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { (*static_cast<f_type**>(obj))->operator()(args...); });
+			call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { (*static_cast<f_type**>(obj))->operator()(args...); }); // NOLINT
 			call.object = &functor;
-			details::ConnectionBase* conn = new details::ConnectionBase(this, idx);
+			auto* conn = new details::ConnectionBase(this, idx); // NOLINT
 			connections.emplace_back(conn);
 			return {conn};
 		} else if constexpr (sizeof(std::remove_pointer_t<f_type>) <= sizeof(void*)) {
 			// copy the functor.
 			size_t idx = connections.size();
 			auto& call = calls.emplace_back();
-			call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { static_cast<f_type*>(obj)->operator()(args...); });
-			new(&call.object) f_type(std::move(functor));
+			call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { static_cast<f_type*>(obj)->operator()(args...); }); // NOLINT
+			new (&call.object) f_type(std::forward<F>(functor));
 			using conn_t = std::conditional_t<std::is_trivially_destructible_v<F>, details::ConnectionBase, details::ConnectionNontrivial<F>>;
-			details::ConnectionBase* conn = new conn_t(this, idx);
+			details::ConnectionBase* conn = new conn_t(this, idx); // NOLINT
 			connections.emplace_back(conn);
 			return {conn};
 		} else {
 			struct unique {
 				f_type* ptr;
 
-				unique(f_type* ptr) : ptr(ptr) {}
-				unique(const unique&) = delete;
+				unique(f_type* ptr) : ptr(ptr) {} // NOLINT
+				unique(unique const&) = delete;
 				unique(unique&&) = delete;
 
 				~unique() { delete ptr; }
@@ -277,9 +295,9 @@ struct Event<void(A...)> : details::EventBase {
 
 			size_t idx = connections.size();
 			auto& call = calls.emplace_back();
-			call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { static_cast<unique*>(obj)->ptr->operator()(args...); });
-			new(&call.object) unique{new f_type(std::move(functor))};
-			details::ConnectionBase* conn = new details::ConnectionNontrivial<unique>(this, idx);
+			call.func = reinterpret_cast<void*>(+[](void* obj, A... args) { static_cast<unique*>(obj)->ptr->operator()(args...); }); // NOLINT
+			new (&call.object) unique{new f_type(std::forward<F>(functor))};
+			details::ConnectionBase* conn = new details::ConnectionNontrivial<unique>(this, idx); // NOLINT
 			connections.emplace_back(conn);
 			return {conn};
 		}
@@ -287,7 +305,7 @@ struct Event<void(A...)> : details::EventBase {
 
 	template <typename F>
 	ConnectionRaw operator+=(F&& functor) const {
-		return connect(functor);
+		return connect(std::forward<F>(functor));
 	}
 };
 
