90c435e009021add7bad7794ec71791db3b0fd17
Ian Pike <Ianpike98@gmail.com>
2025-02-18 22:40:55 -0500

    Fix Ambiguous namespaces

--- game/include/fornani/utils/Math.hpp
+++ game/include/fornani/utils/Math.hpp
@@ -17,7 +17,7 @@ inline sf::Vector2<float> unit(sf::Vector2<float> vec) {
 	if (denominator == 0.f) { return sf::Vector2<float>{1.f, 0.f}; }
 	return vec / denominator;
 }
-inline float direction(sf::Vector2<float> vec) { return atan2f(vec.y, vec.x); }
+inline float direction(sf::Vector2<float> vec) { return std::atan2f(vec.y, vec.x); }
 inline sf::Vector2<float> absolute_distance(sf::Vector2<float> source, sf::Vector2<float> destination) { return sf::Vector2<float>{abs(source.x - destination.x), abs(source.y - destination.y)}; }
 	inline sf::Vector2<float> round_to_even(sf::Vector2<float> input) {
 	auto ret = input;
@@ -30,7 +30,8 @@ inline float ease_in_out(float x, float y, float progress) { return ccm::ext::cu
 inline float ease_out_back(float progress) {
 	auto const c1{1.70158f};
 	auto const c3 = c1 + 1.f;
-	return 1.f + c3 * ccm::pow(progress - 1.f, 3.f) + c1 * ccm::pow(progress - 1.f, 2.f);
+	// TODO: Switch this to ccm::pow once it is finished.
+	return 1.f + c3 * std::pow(progress - 1.f, 3.f) + c1 * std::pow(progress - 1.f, 2.f);
 }
 inline float smoothstep(float x, float y, float progress) { return ccm::ext::smoothstep(x, y, progress); }
 inline bool same_parity(float a, float b) { return ((static_cast<int>(a) ^ static_cast<int>(b)) & 1) == 0; }
