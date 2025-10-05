9a5d457bf5689998fa488266aa9831eb05593298
swagween <109032389+swagween@users.noreply.github.com>
2025-10-05 15:39:02 +0200

    woodshine levels

--- editor/src/metagrid/Room.cpp
+++ editor/src/metagrid/Room.cpp
@@ -47,6 +47,7 @@ void Room::render(sf::RenderWindow& win, sf::Vector2f cam) {
 	m_box.setPosition(get_board_position() + cam);
 	win.draw(m_box);
 	auto sprite = sf::Sprite{m_texture.getTexture()};
+	m_include_in_minimap ? sprite.setColor(sf::Color::White) : sprite.setColor(fornani::colors::periwinkle);
 	sprite.setPosition(m_box.getPosition());
 	sprite.scale({spacing_v / fornani::constants::f_chunk_size, spacing_v / fornani::constants::f_chunk_size});
 	win.draw(sprite);
